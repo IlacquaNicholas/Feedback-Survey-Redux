80dd51cd9ec4a5ac2f36f39b8b704416390e24ae
Nicholas Ilacqua <Niservices88@gmail.com>
2021-12-03 21:24:10 -0600

    finished post route in the server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { Pool } = require('pg');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
@@ -9,7 +10,32 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.post('/review', (req, res)=>{
+    console.log('in POST /review');
+    console.log('req.body:', req.body);
+    const reviewTask = req.body
+    const sqlText = `
+    INSERT INTO "feedback" 
+    ("feeling", "understanding", "support", "comments")
+    VALUES
+    ($1, $2, $3, $4);
+    `;
+    const sqlValue = [
+        reviewTask.feeling,
+        reviewTask.understanding,
+        reviewTask.support, 
+        reviewTask.comments
+    ];
+    Pool.query(sqlText, sqlValue)
+    .then((dbResult)=>{
+        console.log('INSET success');
+        res.sendStatus(200);
+    })
+    .catch((dbErr)=>{
+        console.log(dbErr);
+        res.sendStatus(500);
+    });
+});
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT, () => {
