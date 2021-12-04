a5e85b397eb6bb86098df185207c4d43d217db52
Nicholas Ilacqua <Niservices88@gmail.com>
2021-12-03 19:46:38 -0600

    halfway done with feeling component page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { useDispatch } from 'react-redux';
 import './App.css';
+import FeelingFeedback from '../FeelingFeedback/FeelingFeedback';
 
 function App() {
 
   return (
     <div className='App'>
+      <Router>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+        <Route exact path="/">
+         <FeelingFeedback />
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+//Redux
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
+//This is the Store for the reducers 
+const storeInstance = createStore(
+    combineReducers({
+    
+    }),
+    applyMiddleware(logger),
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
